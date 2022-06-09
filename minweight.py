e7cee12c2fa4b3eca827560b12a9c577c3f92cb2
Tiago de Paula <tiagodepalves@gmail.com>
2022-06-08 21:59:01 -0300

    Reading input

--- minweight.py
+++ minweight.py
@@ -1,6 +1,7 @@
 """Find minimum weight cycle in 'weights.txt'."""
 import os.path
-import re
+import networkx as nx
+import numpy as np
 
 
 def path_to(filename: str):
@@ -15,28 +16,48 @@ def path_to(filename: str):
         return filename
 
 
-def numeric_substring(text: str):
-    """Removes leading '[' and trailing ']' alongside whitespace around it."""
-    return text.strip().lstrip('[').rstrip(']').strip()
+def parse_matrix(text: str):
+    """Parse text as numeric matrix following 'numpy' format."""
+    def numeric_substring(s: str):
+        """Removes leading '[' and trailing ']' alongside whitespace around it."""
+        return s.strip().lstrip('[').rstrip(']').strip()
 
-
-def read_weights(student: str, *, filename: str = path_to('weights.txt')):
-    """Read weights from the provided file."""
-
-    with open(filename, 'r') as file:
-        matrices = (mat.strip() for mat in file.read().split('\n\n'))
-
-    matrix, = (mat for mat in matrices if mat.startswith(student))
-    name, weights = (text.strip() for text in matrix.split('\n', maxsplit=1))
-    assert name == student
-
-    return [
+    return np.asarray([
         [
             float(item.strip())
             for item in numeric_substring(line).split()
         ]
-        for line in numeric_substring(weights).split('\n')
-    ]
+        for line in numeric_substring(text).split('\n')
+    ])
+
+
+def read_weights(*, filename: str = path_to('weights.txt')):
+    """Read weights from the provided file."""
+
+    with open(filename, 'r') as file:
+        contents = file.read()
+        matrices = (mat.strip() for mat in file.read().split('\n\n'))
+
+    weight: dict[str, np.ndarray] = {}
+
+    for text in contents.split('\n\n'):
+        if not (text := text.strip()):
+            continue
+
+        name, matrix = text.split('\n', maxsplit=1)
+        weight[name.strip()] = parse_matrix(matrix)
+
+    return weight
+
+
+def read_graph(student: str) -> nx.Graph:
+    """Read weighted graph for the given student."""
+    weights = read_weights()
+    names = {idx: name for idx, name in enumerate(weights.keys())}
+
+    graph = nx.from_numpy_matrix(weights[student])
+    return nx.relabel_nodes(graph, names)
+
 
 
 if __name__ == '__main__':
@@ -44,12 +65,11 @@ if __name__ == '__main__':
 
     # arguments
     parser = ArgumentParser('minweight.py')
-    parser.add_argument('student', metavar='NAME', nargs='?',
-        help='Student to solve.')
-
-    args = parser.parse_args()
-    student = args.student or 'Ti'
+    parser.add_argument('-d', '--draw', action='store_true',
+        help='draw NetworkX graph using Matplotlib')
 
     # reading input
-    weight_matrix = read_weights(student)
-    raise NotImplementedError(weight_matrix)
+    args = parser.parse_args()
+    graph = read_graph('Ti')
+
+    raise NotImplementedError(graph)
